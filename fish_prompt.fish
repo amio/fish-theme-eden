e5a422d6834c55176ec6c380c90d375c97218d3a
Meeeeow <os@async.sh>
2019-09-29 16:20:31 +0800

    Add eden_toggle_ssh_tag command

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -33,11 +33,13 @@ function _prompt_segment -d "Function to show a segment"
 end
 
 function show_ssh_status -d "Function to show the ssh tag"
-  if [ -n "$SSH_CLIENT" ]
-    if [ (id -u) = "0" ]
-      _prompt_segment red white "-SSH-" ' '
-    else
-      _prompt_segment blue white "-SSH-" ' '
+  if test "$THEME_EDEN_HIDE_SSH_TAG" != 'yes'
+    if [ -n "$SSH_CLIENT" ]
+      if [ (id -u) = "0" ]
+        _prompt_segment red white "-SSH-" ' '
+      else
+        _prompt_segment blue white "-SSH-" ' '
+      end
     end
   end
 end
